a61b2fc333450cd83920521f76e999af65ce1783
Kritsada Chan-inn <bankth99@gmail.com>
2025-09-15 00:14:29 +0700

    Frontend product API

---
+++ frontend/src/components/TestFetch.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { API_URL } from "../libs/api";
+import { getUser, createUser, updateUser, deleteUser } from "../libs/fetchUserUtils";
+import { getProducts, createProduct } from "../libs/productService";
+
+function TestFetch() {
+
+    let testObj;
+    let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4YzZlY2E3M2QyZjg5ZjAxZmM1ZDJkNSIsImlhdCI6MTc1Nzg2NzE3NiwiZXhwIjoxNzYwNDU5MTc2fQ.Z-f7iKzRZuwYnVdZ8osKauFpUHYzYAKyxXKTpZuMSh0";
+
+    useEffect(() => {
+    const fetchTest = async () => {
+      // testObj = await getProducts(API_URL);
+      // users = await getUser(API_URL);
+      // console.log(testObj);
+      // console.log(await updateUser(API_URL, "68ad16333b6e594e92e48e04", {username: "test1"}));
+      // console.log(await deleteUser(API_URL, "68ad16333b6e594e92e48e04"));
+      console.log(
+        await createProduct(API_URL, token, 
+          {
+          product_name: "asadsadasd",
+          stock_quantity: 69,
+          price: 69,
+          rating: 4.5,
+          vid:"asdsad",
+          type: "",
+          product_description:"asdasd"
+        })
+      );
+    };
+
+    fetchTest();
+  }, []);
+
+  return (
+    <>
+      {testObj}
+    </>
+  );
+}
+
+export default TestFetch;
---
+++ frontend/src/libs/productService.js
@@ -0,0 +1,42 @@
+const getProducts = async (url) => {
+  try {
+    const data = await fetch(`${url}/products`);
+    const products = data.json();
+    return products;
+  } catch (err) {
+    throw new Error("Can not get products");
+  }
+}
+const createProduct = async (url,token, newProduct ) => {
+  console.log("Create Product Called")
+  try {
+    console.log(JSON.stringify(newProduct))
+    const res = await fetch(`${url}/products`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        ...newProduct,
+      }),
+    });
+    const createdProduct = await res.json();
+    return createdProduct;
+  } catch (e) {
+    console.log(e);
+    throw new Error("Can not create product");
+  }
+};
+const deleteProduct = async (url, id) => {
+  try {
+    const res = await fetch(`${url}/Product/${id}`, {
+      method: "DELETE"
+    });
+    return res.status;
+  } catch (e) {
+    throw new Error("Can not delete product");
+  }
+}
+
+export { getProducts, createProduct }
