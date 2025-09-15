776970ebe573d0a546a128dd73771e1cdd23cde0
miramsalp <Thanapat_petch@hotmail.com>
2025-09-16 01:48:41 +0700

    backend: add upload route prepare for frontend

--- backend/app.js
+++ backend/app.js
@@ -35,6 +35,10 @@ const categoryRouter = require("./routes/category-routes");
 const otpRoutes = require('./routes/otp-routes');
 const vendorAppRoutes = require('./routes/vendor-application-routes');
 const adminRoutes = require('./routes/admin-routes');
+const uploadRoutes = require('./routes/upload-routes');
+
+//Mount upload route
+app.use('/api/v1/Agaya/upload', uploadRoutes);
 
 //Mount routers
 app.use("/api/v1/Agaya/auth", authRouter);  
