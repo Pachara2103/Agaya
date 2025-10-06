4ebdb249a1f755c1c79357abe23e163a5cbb2636
Tanwong Pattamamongkolchai <kruppomm@gmail.com>
2025-10-06 13:38:00 +0700

    add some document

--- backend/controllers/order-controller.js
+++ backend/controllers/order-controller.js
@@ -80,6 +80,7 @@ exports.checkoutOrder = async (req, res) => {
 };
 
 //PATCH /api/v1/agaya/orders/:orderId/status
+//req.body = { "status" : "PAID"}
 exports.updateOrderStatus = async (req, res) => {
   try {
     const { orderId } = req.params;
@@ -209,8 +210,38 @@ exports.getOrdersByCustomer = async (req, res) => {
       return res.status(404).json({ message: `No orders belong to user ${cid}` });
     }
 
-    res.status(200).json({ orders });
+    res.status(200).json({success:true, orders });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
+/* Output example
+{
+    "success": true,
+    "orders": [
+        {
+            "_id": "68e22c2338854b09f61b5ed7",
+            "order_status": "PAID",
+            "order_date": "2025-10-05T08:28:19.168Z",
+            "customer_id": "68e227cec54f687261180ce9",
+            "contains": [
+                {
+                    "product_id": "68c6f90d42c4d92f49b2b6e6",
+                    "name": "Sample 2Product",
+                    "price": 199.99,
+                    "quantity": 4,
+                    "total_price": 799.96
+                },
+                {
+                    "product_id": "68c6f9136b57f2998888277b",
+                    "name": "เครื่องนวดท่านชายสีดำ",
+                    "price": 399,
+                    "quantity": 3,
+                    "total_price": 1197
+                }
+            ],
+            "order_total": 1996.96
+        }
+    ]
+}
+*/
