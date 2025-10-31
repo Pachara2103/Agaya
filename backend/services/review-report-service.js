afc0822e1835f54d9890a06ce84495cb312ae6f6
Tanwong Pattamamongkolchai <kruppomm@gmail.com>
2025-10-31 08:12:56 +0700

    add change report status

--- backend/services/review-report-service.js
+++ backend/services/review-report-service.js
@@ -1,4 +1,6 @@
 const ReviewReport = require("../models/review-report");
+const {deleteReview} = require("./review-service");
+const auditService = require('./audit-service');
 const createError = require("http-errors")
 
 exports.createReviewReport = async (data) => {
@@ -15,6 +17,7 @@ exports.getAllReviewReports = async (filters = {}) => {
     let query = {};
     if(filters.vendorId) query.vendorId = filters.vendorId;
     if(filters.status) query.status = filters.status;
+    if(filters.reason) query.reason = filters.reason;
     
     return await ReviewReport.find(query)
       .populate("reviewId")
@@ -28,21 +31,8 @@ exports.getReviewReportById = async (id) => {
   try {
     const report = await ReviewReport.findById(id)
       .populate("reviewId")
-      .populate("vendorId");
     if (!report) throw new createError(404, "Review report not found");
     return report;
-  } catch (err) {
-    throw(err);
-  }
-};
-
-exports.updateReviewReport = async (id, data) => {
-  try {
-    const updated = await ReviewReport.findByIdAndUpdate(id, data, {
-      new: true,
-    });
-    if (!updated) throw new createError(404, "Review report not found");
-    return updated;
   } catch (err) {
     throw(err);
   }
@@ -58,9 +48,9 @@ exports.deleteReviewReport = async (id) => {
   }
 };
 
-exports.updateReviewReportStatus = async(id, status) => {
+exports.updateReviewReportStatus = async(id, status, user) => {
     try{
-        validStatus = ["REJECTED", "APPROVED"];
+        const validStatus = ["REJECTED", "APPROVED"];
         if (!validStatuses.includes(status)) {
             throw new createError(
                 400, `Invalid status. Valid values: ${validStatus.join(", ")}`
@@ -71,10 +61,15 @@ exports.updateReviewReportStatus = async(id, status) => {
             throw new createError(404, "Review report not found");
         }
         if(reviewReport.status !== "PENDING"){
-            throw new createError(400, "Report is already approved ar rejected");
+            throw new createError(400, "Report is already approved or rejected");
+        }
+        if(status == "APPROVED"){
+            await deleteReview(reviewReport.reviewId, user);
         }
         reviewReport.status = status;
+        reviewReport.adminResponseDate = new Date();
         await reviewReport.save();
+        await auditService.logAction(user._id, "update", "ReviewReport", reviewReport._id, reviewReport);
         return reviewReport;
     } catch (err) {
         throw(err);
