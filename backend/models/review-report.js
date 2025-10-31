d7f7acb9e5f3d3f4d193444ac94fc4a65e29d946
Tanwong Pattamamongkolchai <kruppomm@gmail.com>
2025-10-31 07:41:07 +0700

    add review report schems

---
+++ backend/models/review-report.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const reviewReportSchema = new mongoose.Schema(
+  {
+    reviewId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review",
+      required: true,
+    },
+    vendorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vendor",
+      required: true,
+    },
+    reason: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String,
+      enum: ["PENDING", "REJECTED", "APPROVED"],
+      default: "PENDING",
+    },
+    adminResponseAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
