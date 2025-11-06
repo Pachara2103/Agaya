6ff059bd61c596a20c82af4926ecea627bdfd1bc
miramsalp <Thanapat_petch@hotmail.com>
2025-11-06 12:25:24 +0700

    change title to black

--- frontend/src/components/SellerPage/SellerConfirmationModal.jsx
+++ frontend/src/components/SellerPage/SellerConfirmationModal.jsx
@@ -4,7 +4,7 @@ const SellerConfirmationModal = ({ open, onConfirm, onCancel, title, message }) 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 bg-opacity-50">
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full text-center">
-        <h2 className="text-2xl font-semibold mb-4">{title}</h2>
+        <h2 className="text-2xl text-gray-900 font-semibold mb-4">{title}</h2>
         <p className="text-gray-700 mb-8">{message}</p>
         <div className="flex justify-center gap-4">
           <button
